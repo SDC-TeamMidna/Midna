d8688b841c81ad0a8775cb05e0272da53c938918
Bishal Gautam <bgautam.se@gmail.com>
2021-10-02 14:56:16 -0700

    reviews query with response and empty array

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -11,10 +11,12 @@ module.exports = {
     const qsort = sortOptions[sort];
     const qpage = (page - 1) * count;
     const qparams = [product_id, count, qpage];
-    console.log(qparams, 'sort params');
-    const query = `SELECT reviews.id as review_id, rating, summary, recommend, body,
+    // console.log(qparams, 'sort params');
+    const query = `SELECT reviews.id as review_id, rating, summary, recommend,
+    CASE WHEN reviews.response = 'null' THEN NULL ELSE reviews.response END as response, body,
     date, reviewer_name, helpfulness,
-    JSON_AGG(json_build_object('id', reviews_photos.id, 'url', reviews_photos.url) ORDER BY reviews_photos.id ASC) as photos
+    COALESCE(JSON_AGG(json_build_object('id', reviews_photos.id, 'url', reviews_photos.url)
+    ORDER BY reviews_photos.id ASC) FILTER (WHERE reviews_photos.url IS NOT NULL), '[]') as photos
     FROM public.reviews LEFT JOIN public.reviews_photos ON reviews.id = reviews_photos.review_id
     WHERE product_id=$1
     GROUP BY reviews.id
@@ -27,3 +29,20 @@ module.exports = {
   //post reviews query
 
 };
+
+// COALESCE(JSON_AGG(json_build_object('id', reviews_photos.id, 'url', reviews_photos.url)
+// ORDER BY reviews_photos.id ASC) FILTER (WHERE reviews_photos.url IS NOT NULL), '[]') as photos
+
+// SELECT * FROM (SELECT reviews.id
+//   as review_id, rating, summary, recommend,
+//   CASE WHEN reviews.response = 'null' THEN NULL ELSE reviews.response END as response,
+//   body, date, reviewer_name, helpfulness,
+//   COALESCE(JSON_AGG(json_build_object('id', reviews_photos.id, 'url', reviews_photos.url)
+//   ORDER BY reviews_photos.id ASC) FILTER (WHERE reviews_photos.url IS NOT NULL), '[]') as photos
+//   FROM public.reviews LEFT JOIN public.reviews_photos
+//   ON reviews.id = reviews_photos.review_id
+//   WHERE product_id=40355
+//   GROUP BY reviews.id
+//   OFFSET 3 ROWS
+//   FETCH FIRST 3 ROWS ONLY) AS data
+//   ORDER BY data.helpfulness DESC
