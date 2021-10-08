2ba09766df2d91732c40ec372495512d4244ecb9
Bishal Gautam <bgautam.se@gmail.com>
2021-10-08 14:53:49 -0700

    default port 3000

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const router = require('./routes/routes');
 require('dotenv').config({ path: path.join(__dirname, '/config/.env') });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
