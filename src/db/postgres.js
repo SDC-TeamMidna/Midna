515d63fe625c550695334feef4eeb4cf9a99ece2
Bishal Gautam <bgautam.se@gmail.com>
2021-12-20 20:09:18 -0800

    process env cleanup

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 require('dotenv').config({ path: path.join(__dirname, '../config/.env') });
 
 const db = new Pool({
-  user: 'ubuntu',
-  host: '3.17.163.41',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.PG_DB,
   password: process.env.DB_PASS2,
 });
